101c62b0e97695c318e81052d46adf552d6c202a
rafiiwicaksono <rafiihsan5@gmail.com>
2023-12-19 21:59:43 +0700

    server-register

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,14 @@
+const {User} = require(`../models`)
 class UserController {
-
+    static async register(req, res, next) {
+        try {
+            const {username, email, password} = req.body
+            const user = await User.create({username, email, password})
+            res.status(201).json({"id": user.id, "email": user.email})
+        } catch (error) {
+            next(error)
+        }
+    }
 }
 
 module.exports = UserController
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const errorHandler = require('../middlewares/errorHandler')
+const UserController = require('../controllers/UserController')
 const router = express.Router()
+
+
+router.post(`/register`, UserController.register)
+
+
 
 
 router.use(errorHandler)
